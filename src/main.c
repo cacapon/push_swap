e78fe82040f7bb8e54f2091896cfcfadfe20d2b6
Cacapon Project <cacapon.project@gmail.com>
2024-12-23 21:04:18 +0900

    プロトタイプ作成

--- src/main.c
+++ src/main.c
@@ -1,30 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/23 20:32:57 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/12/23 20:54:33 by ttsubo           ###   ########.fr       */
+/*   Created: 2024/12/23 20:47:52 by ttsubo            #+#    #+#             */
+/*   Updated: 2024/12/23 20:47:57 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#include "push_swap.h"
 
-// enum
-typedef enum e_stack_type
+int	main(int argc, char **argv)
 {
-	STACK_A,
-	STACK_B,
-	STACK_BOTH,
-}				t_stack_type;
+	(void) argc;
+	(void) argv;
 
-// prototype
-void swap(int *stack, t_stack_type type);
-void push(int *from_stack, int *to_stack, t_stack_type type);
-void rotate(int *stack, t_stack_type type);
-void rv_rotate(int *stack, t_stack_type type);
-
-#endif
+	return (0);
+}
--- src/push_swap.c
+++ src/push_swap.c
@@ -6,16 +6,60 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/23 20:41:56 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/12/23 20:43:46 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/12/23 21:03:40 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(int argc, char **argv)
+/**
+ * @brief stackの最初の二つの要素を入れ替えます
+ * 
+ * @param [out] stack	: 要素を入れ替えるスタック
+ * @param [in] type		: スタックの種類
+ * @note 				: 要素数が1以下の場合は操作しません
+ */
+void swap(int *stack, t_stack_type type)
 {
-	(void) argc;
-	(void) argv;
+	(void) stack;
+	(void) type;
+}
 
-	return (0);
+/**
+ * @brief from -> toへスタックの先頭の要素を送ります
+ * 
+ * @param from_stack	: 送り元のスタック
+ * @param to_stack		: 送り先のスタック
+ * @param type			: スタックの種類
+ * @note				: fromが空の場合は操作しません
+ */
+void push(int *from_stack, int *to_stack, t_stack_type type)
+{
+	(void) from_stack;
+	(void) to_stack;
+	(void) type;
+}
+
+/**
+ * @brief スタックを１つずつシフトアップします。最初の要素が最後の要素になります。
+ * 
+ * @param stack		: シフトアップするスタック
+ * @param type		: スタックの種類
+ */
+void rotate(int *stack, t_stack_type type)
+{
+	(void) stack;
+	(void) type;
+}
+
+/**
+ * @brief スタックを１つずつシフトダウンします。最後の要素が最初の要素になります。
+ * 
+ * @param stack		: シフトアップするスタック
+ * @param type		: スタックの種類
+ */
+void rv_rotate(int *stack, t_stack_type type)
+{
+	(void) stack;
+	(void) type;
 }
