89722da9308ef6d0abd6f9f787ccd19b17f2ce06
cacapon <takuma.tsubo@amazingengine.co.jp>
2025-01-08 13:01:25 +0900

    norm修正

--- src/main.c
+++ src/main.c
@@ -6,81 +6,34 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/23 20:47:52 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/01/08 10:03:05 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/01/08 13:00:57 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int	ft_atoi_with_error(const char *str, int *error)
-{
-	long int	sign;
-	long int	num;
-
-	sign = 1;
-	num = 0;
-	*error = 0;
-	while (*str == ' ')
-		str++;
-	if (*str == '-')
-		sign = -1;
-	if (*str == '+' || *str == '-')
-		str++;
-	while (ft_isdigit(*str))
-	{
-		num = num * 10 + (*str - '0');
-		if (num * sign > INT_MAX || num * sign < INT_MIN)
-			return (*error = 1, 0);
-		str++;
-	}
-	if (*str != '\0')
-		return (*error = 1, 0);
-	return ((int)(sign * num));
-}
-
-static char	**free_tokens(char **tokens, size_t index)
-{
-	while (index > 0)
-		free(tokens[--index]);
-	free(tokens);
-	return (NULL);
-}
-
-/**
- * @brief
- *
- * @param arg
- * @return int
- */
-static t_psh_swp_sts	validate_input(char *input)
-{
-	char	**tokens;
-	int		error;
-	int		i;
-
-	tokens = ft_split(input, ' ');
-	if (!tokens)
-		return (PSH_SWP_NG);
-	i = 0;
-	while (tokens[i] != NULL)
-	{
-		error = 0;
-		ft_atoi_with_error(tokens[i], &error);
-		if (error)
-			return (free_tokens(tokens, i), PSH_SWP_NG);
-		i++;
-	}
-	free_tokens(tokens, i);
-	return (PSH_SWP_OK);
-}
-
 int	main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
-	if (validate_input(argv[1]) == 0)
-		ft_printf("OK");
-	else
-		ft_printf("NG");
+	t_dll	*stack_a;
+	t_dll	*stack_b;
+	int		i;
+	int		value;
+
+	if (argc <= 1)
+		return (ft_printf("Error\n"), 1);
+	stack_a = ft_calloc(sizeof(t_dll), 1);
+	stack_b = ft_calloc(sizeof(t_dll), 1);
+	stack_a = init_dll();
+	stack_b = init_dll();
+	i = 1;
+	while (i < argc)
+	{
+		value = ft_atoi(argv[i++]);
+		if (stack_a->is_in(stack_a, value))
+			return (ft_printf("Error\n"), 1);
+		stack_a->add(stack_a, value);
+	}
+	stack_a->free(&stack_a);
+	stack_b->free(&stack_b);
 	return (0);
 }
