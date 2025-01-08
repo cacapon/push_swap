eb92cd054387ec99022b1ef8dd9ecf1a5c920e80
cacapon <takuma.tsubo@amazingengine.co.jp>
2025-01-08 12:58:33 +0900

    ヘッダー修正

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/23 20:32:57 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/12/24 11:52:14 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/01/08 11:55:40 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define PUSH_SWAP_H
 
 # include "../lib/libft/libft.h"
+# include "doubly_linked_list.h"
 
 // enum
 typedef enum e_stack_type
@@ -22,12 +23,6 @@ typedef enum e_stack_type
 	STACK_B,
 	STACK_BOTH,
 }		t_stack_type;
-
-typedef enum e_psh_swp_sts
-{
-	PSH_SWP_OK,
-	PSH_SWP_NG,
-}		t_psh_swp_sts;
 
 // prototype
 void	swap(int *stack, t_stack_type type);
