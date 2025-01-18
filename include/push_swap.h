5f4839aef55b69c828b46241c0a99a944a119a33
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2025-01-18 14:02:47 +0900

    選択挿入ロジック作成

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/23 20:32:57 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/01/17 14:58:55 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/01/18 13:43:00 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,34 +17,44 @@
 # include "doubly_linked_list.h"
 
 // enum
-typedef enum e_stack_type
-{
-	STACK_A,
-	STACK_B,
-	STACK_BOTH,
-}		t_stack_type;
 
 typedef enum e_push_swap_err
 {
 	PSW_OK,
 	ERR_PSW_OUTOFRANGE,
 	ERR_PSW_NOT_NUM,
-}		t_push_swap_err;
+}			t_push_swap_err;
+
+// struct
+
+typedef struct e_move_count
+{
+	size_t	left;
+	size_t	right;
+}			t_move_count;
+
+typedef struct e_move_cmd
+{
+	size_t	a_r;
+	size_t	a_l;
+	size_t	b_r;
+	size_t	b_l;
+}			t_move_cmd;
 
 // prototype
 
-int		ft_atoi_with_error(const char *str, t_push_swap_err *error);
-void	pa(t_dll *stack_a, t_dll *stack_b);
-void	pb(t_dll *stack_b, t_dll *stack_a);
-void	ra(t_dll *stack_a);
-void	rb(t_dll *stack_b);
-void	rr(t_dll *stack_a, t_dll *stack_b);
-void	rra(t_dll *stack_a);
-void	rrb(t_dll *stack_b);
-void	rrr(t_dll *stack_a, t_dll *stack_b);
-void	sa(t_dll *stack_a);
-void	sb(t_dll *stack_b);
-void	ss(t_dll *stack_a, t_dll *stack_b);
-void	bubble_sort(t_dll *stack_a);
-void	select_sort(t_dll *stack_a, t_dll *stack_b);
+int			ft_atoi_with_error(const char *str, t_push_swap_err *error);
+void		pa(t_dll *stack_a, t_dll *stack_b);
+void		pb(t_dll *stack_b, t_dll *stack_a);
+void		ra(t_dll *stack_a);
+void		rb(t_dll *stack_b);
+void		rr(t_dll *stack_a, t_dll *stack_b);
+void		rra(t_dll *stack_a);
+void		rrb(t_dll *stack_b);
+void		rrr(t_dll *stack_a, t_dll *stack_b);
+void		sa(t_dll *stack_a);
+void		sb(t_dll *stack_b);
+void		ss(t_dll *stack_a, t_dll *stack_b);
+void		bubble_sort(t_dll *stack_a);
+void		select_sort(t_dll *stack_a, t_dll *stack_b);
 #endif
--- src/logic/select_insert_sort.c
+++ src/logic/select_insert_sort.c
@@ -6,46 +6,131 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 17:27:46 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/01/17 15:31:42 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/01/18 14:01:42 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static size_t	_get_insert_index(t_dll *stack_b, int value)
+static t_move_count	_get_move_count_a(t_dll *stack, t_dll_node *node)
 {
-	size_t		index;
-	t_dll_node	*node;
+	t_move_count	mv;
 
-	index = 0;
-	node = stack_b->head;
-	while (node)
+	mv.right = stack->index(stack, node->value);
+	mv.left = (stack->len(stack) - mv.right) % stack->len(stack);
+	return (mv);
+}
+
+/**
+ * @brief スタックｂにインサートするために移動させる量を計算します
+ *
+ * @param stack
+ * @param node
+ * @return t_move_count
+ * @note	見つからない場合は最小値の前の位置に移動する量を返します。
+ */
+static t_move_count	_get_move_count_b(t_dll *stack, t_dll_node *node)
+{
+	t_dll_node		*check;
+	t_move_count	mv;
+	const int		value = node->value;
+
+	mv = (t_move_count){.left = 0, .right = 0};
+	if (stack->len(stack) <= 1)
+		return (mv);
+	check = stack->head;
+	while (check)
 	{
-		if (node->value > value && value > node->next->value)
-			return (index);
-		node = node->next;
-		index++;
-		if (node == stack_b->head)
+		if (check->value < value && value < check->next->value)
 			break ;
+		check = check->next;
+		if (check == stack->head)
+		{
+			mv.right = stack->index(stack, stack->min(stack));
+			break ;
+		}
+		mv.right++;
 	}
-	exit(1);
+	mv.left = (stack->len(stack) - mv.right) % stack->len(stack);
+	return (mv);
+}
+
+static t_move_cmd	_get_min_move(t_move_count mv_a, t_move_count mv_b)
+{
+	t_move_cmd	result;
+
+	result = (t_move_cmd){.a_l = 0, .a_r = 0, .b_l = 0, .b_r = 0};
+	if (mv_a.right < mv_a.left)
+		result.a_r += mv_a.right;
+	else
+		result.a_l += mv_a.left;
+	if (mv_b.right < mv_b.left)
+		result.b_r += mv_b.right;
+	else
+		result.b_l += mv_b.left;
+	return (result);
+}
+
+static size_t	_get_min_cmd_index(t_move_cmd *mv, size_t len)
+{
+	size_t i;
+	size_t mv_min;
+	size_t mv_min_i;
+	size_t total;
+
+	i = 0;
+	mv_min = ULONG_MAX;
+	mv_min_i = 0;
+	while (i < len)
+	{
+		total = mv[i].a_l + mv[i].a_r + mv[i].b_l + mv[i].b_r;
+		if (total < mv_min)
+		{
+			mv_min = total;
+			mv_min_i = i;
+		}
+		i++;
+	}
+	return (mv_min_i);
 }
 
 void	select_insert_sort(t_dll *stack_a, t_dll *stack_b)
 {
-	size_t	a_min_index;
+	size_t		index;
+	t_move_cmd	*mv;
+	t_dll_node	*node;
+	size_t		mv_min;
+	size_t		cmd_i;
 
-	// aすべての要素の移動量の最小を見つける
-		// WIP aの最小の移動量の見つけ方
-			// stack_a->min(self)で最小値をゲット
-			// stack_a->index(min)でminの＞向きの移動量ゲット
-			// (stack_a->len(self) - a_min) % stack-a->len(self)で＜向きの移動量ゲット
-		// WIP ｘに対するbの最小の移動量の見つけ方
-			// target > x && x > target.next でいけそう
-			// (stack_b->len(self) - index) % stack-b->len(self)で逆向きの移動量取得
-		// ４つの移動量の組み合わせから最小を取得
-			// TODO 同じ向きは重ねることで効率化できる
-	// 全ての候補の最小移動量で最も移動量の少ないものを選択
-	// その動きに沿って移動させる
-	// bからaに戻す
+	while (stack_a->head)
+	{
+		node = stack_a->head;
+		index = 0;
+		mv = ft_calloc(stack_a->len(stack_a), sizeof(t_move_count));
+		if (!mv)
+			exit(1);
+		while (index < stack_a->len(stack_a))
+		{
+			mv[index] = _get_min_move(_get_move_count_a(stack_a, node),
+					_get_move_count_b(stack_b, node));
+			node = node->next;
+			index++;
+		}
+		cmd_i = _get_min_cmd_index(mv, stack_a->len(stack_a));
+		while (mv[cmd_i].a_l || mv[cmd_i].a_r || mv[cmd_i].b_l || mv[cmd_i].b_r)
+		{
+			if(mv[cmd_i].a_l && mv[cmd_i].a_l--)
+				rra(stack_a);
+			if(mv[cmd_i].a_r && mv[cmd_i].a_r--)
+				ra(stack_a);
+			if(mv[cmd_i].b_l && mv[cmd_i].b_l--)
+				rrb(stack_a);
+			if(mv[cmd_i].b_r && mv[cmd_i].b_r--)
+				rb(stack_a);
+		}
+		pb(stack_b, stack_a);
+		free(mv);
+	}
+	while (stack_b->head)
+		pa(stack_a, stack_b);
 }
