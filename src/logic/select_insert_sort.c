1a119843335cd84871d6762dc863e5b5f9043f04
Cacapon Project <cacapon.project@gmail.com>
2025-01-19 21:37:14 +0900

    lenを変数で持って処理回数を削減

--- src/logic/select_insert_sort.c
+++ src/logic/select_insert_sort.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 17:27:46 by ttsubo            #+#    #+#             */
-/*   Updated: 2025/01/19 20:56:34 by ttsubo           ###   ########.fr       */
+/*   Updated: 2025/01/19 21:30:14 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,17 +27,19 @@ void	select_insert_sort(t_dll *stack_a, t_dll *stack_b)
 	size_t		ite;
 	t_cmd_mv	*cmd_moves;
 	t_cmd_mv	min_mv;
+	size_t		a_len;
 
 	while (stack_a->head)
 	{
 		ite = 0;
-		cmd_moves = ft_calloc(stack_a->len(stack_a), sizeof(t_cmd_mv));
-		while (ite < stack_a->len(stack_a))
+		a_len = stack_a->len(stack_a);
+		cmd_moves = ft_calloc(a_len, sizeof(t_cmd_mv));
+		while (ite < a_len)
 		{
 			cmd_moves[ite] = get_cmd_mv(ite, stack_a, stack_b);
 			ite++;
 		}
-		min_mv = get_min_mv(cmd_moves, stack_a->len(stack_a));
+		min_mv = get_min_mv(cmd_moves, a_len);
 		exec_cmd(&stack_a, &stack_b, min_mv);
 		free(cmd_moves);
 	}
